d5e3938406a0a8e68b2e6c4b3b87be7ffd6a0df7
Tugui Madalina <tuguimadalinaa@gmail.com>
2020-10-24 16:50:50 +0300

    updated CBC -to be done encr byte by byte

--- TemaSI1/CBC.py
+++ TemaSI1/CBC.py
@@ -1,6 +1,9 @@
+import base64
+
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+from Crypto.Util.Padding import pad, unpad
 import os
+import numpy as np
 
 # https://stackoverflow.com/questions/29408173/byte-operations-xor-in-python
 # iv = Crypto.Random.get_random_bytes(AES.block_size)
@@ -11,15 +14,25 @@ key = b'\xcd\xd4\xf0#\xb5&Sh\x1f\x0bleaD.\xe7'
 
 
 def xor_op(plaintext, the_xorer):
-    return [a ^ b for (a, b) in zip(plaintext, the_xorer)]
+    return [a ^ b for a, b in zip(plaintext, the_xorer)]
 
 
-def key_encrypt(xored_text):
-    return [a ^ b for (a, b) in zip(xored_text, key)]
+def key_encrypt(message):
+    print("Inainte de encrypt: ", message)
+    message = ''.join(map(str, message))
+    raw = pad(message.encode(), 16)
+    cipher = AES.new(key, AES.MODE_ECB)
+    enc = cipher.encrypt(raw)
+    print("Enc:", enc)
+    return enc
 
 
-def key_decrypt(xored_text):
-    return [a ^ b for (a, b) in zip(xored_text, key)]
+def key_decrypt(enc):
+    raw = pad(enc, 16)
+    cipher = AES.new(key, AES.MODE_ECB)
+    dec = cipher.decrypt(raw)
+    print("Dec: ", dec)
+    return dec
 
 
 def encrypt(plaintext):
@@ -29,10 +42,10 @@ def encrypt(plaintext):
     start = 0
     end = 16
     if end > len(bytes_plaintext):
-        xored_array = xor_op(bytes_plaintext[start:len(bytes_plaintext)], iv)
-        encrypted_elements.append(key_encrypt(xored_array))
+        xored = xor_op(bytes_plaintext[start:len(bytes_plaintext)], iv)
+        encrypted_elements.append(key_encrypt(xored))
         return encrypted_elements
-    while end < len(bytes_plaintext):
+    while end > len(bytes_plaintext):
         if number_of_elements_encrypted == 0:
             xored_array = xor_op(bytes_plaintext[start:end], iv)
             number_of_elements_encrypted += 1
@@ -55,27 +68,31 @@ def decrypt(array_cipher):
     nr_of_decr = 0
     last_element = None
     for element in array_cipher:
+        print("Elementul trimis la decriptat: ", element)
         decr = key_decrypt(element)
+        print("Elementul decriptat: ", decr)
         if nr_of_decr == 0:
-            result = xor_op(decr, iv)
-            decr_elem.append(bytes(result))
+            result = xor_op([30, 128, 204], iv)
+            decr_elem.append(result)
             nr_of_decr += 1
         else:
             result = xor_op(decr, last_element)
-            decr_elem.append(bytes(result))
+            decr_elem.append(result)
         last_element = element
     return decr_elem
 
 
-result = encrypt("ana are mere rosii")
-print("Encrypted text: ", result)
+result = encrypt("ana")
+# print("Encrypted text: ", len(result[0]))
 decr = decrypt(result)
-string = ""
-for x in decr:
-    string += x.decode()
+string= ""
+print(decr[0])
+for x in decr[0]:
+    string += chr(x)
 print("Decrypted text: ", string)
 
 # aes = AES.new(key, AES.MODE_CBC, iv)
 # # list = "".join(str(x) for x in xored_text)
 # # return aes.decrypt(pad(list.encode(), AES.block_size))
 # return aes.decrypt(pad(xored_text, AES.block_size))
+# https://stackoverflow.com/questions/51884553/aes-ecb-encrypting-in-python
